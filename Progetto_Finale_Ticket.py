7322d1044eb68c25a71f92e437d3d51a7aa58c62
Joaquim Francalanci <jofrancalanci@gmail.com>
2024-07-14 09:58:27 +0200

    Search Concerts Menu

--- Progetto_Finale_Ticket.py
+++ Progetto_Finale_Ticket.py
@@ -2,7 +2,7 @@ import os
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 import time
-from fuzzywuzzy import process # Trova artisti simili di nome a quello inserito
+from fuzzywuzzy import process
 
 # Connessione iniziale a MongoDB
 uri = "mongodb+srv://mongo:mongo@ufs13.9ag482l.mongodb.net/?retryWrites=true&w=majority&appName=UFS13"
@@ -27,8 +27,20 @@ def search_concerts_by_artist(artist_name):
     concerts = collection.find({"artisiti": artist_name})
     return concerts
 
+# Funzione per cercare i concerti per nome del concerto
+def search_concerts_by_name(concert_name):
+    collection = get_collection()
+    concerts = collection.find({"nome": concert_name})
+    return concerts
+
+# Funzione per cercare i concerti per località
+def search_concerts_by_location(location_name):
+    collection = get_collection()
+    concerts = collection.find({"nome_luogo": location_name})
+    return concerts
+
 # Funzione per cercare artisti simili
-def find_similar_artist(artist_name): # Provare con "SferaEbbasta"
+def find_similar_artist(artist_name): 
     collection = get_collection()
     artists = collection.distinct("artisiti")
     similar_artist, score = process.extractOne(artist_name, artists)
@@ -50,21 +62,55 @@ def display_concerts(concerts):
         print("--------------")
     
     if not found:
-        print("Nessun concerto trovato per l'artista specificato.")
+        print("Nessun concerto trovato per la ricerca specificata.")
 
 # Funzione per cercare concerti
 def search_concerts_menu():
-    artist_name = input("Inserisci il nome dell'artista da cercare: ")
-    collection = get_collection()
-    concerts = search_concerts_by_artist(artist_name)
-    if collection.count_documents({"artisiti": artist_name}) == 0:
-        similar_artist = find_similar_artist(artist_name)
-        if similar_artist:
-            print(f"Forse cercavi {similar_artist}")
-            concerts = search_concerts_by_artist(similar_artist)
-    clear_screen()
-    display_concerts(concerts)
-    input("\nPremi INVIO per tornare al menu principale...")
+    while True:
+        clear_screen()
+        print("Cerca concerti")
+        print("==============")
+        print("1. Cerca per artista")
+        print("2. Cerca per nome del concerto")
+        print("3. Cerca per località")
+        print("4. Torna al menu principale")
+        
+        scelta = input("\nInserisci il numero dell'opzione desiderata: ")
+        clear_screen()
+        
+        if scelta == '1':
+            artist_name = input("Inserisci il nome dell'artista da cercare: ")
+            collection = get_collection()
+            concerts = search_concerts_by_artist(artist_name)
+            if collection.count_documents({"artisiti": artist_name}) == 0:
+                similar_artist = find_similar_artist(artist_name)
+                if similar_artist:
+                    print(f"Forse cercavi {similar_artist}")
+                    concerts = search_concerts_by_artist(similar_artist)
+            clear_screen()
+            display_concerts(concerts)
+            input("\nPremi INVIO per tornare al menu di ricerca...")
+
+        elif scelta == '2':
+            concert_name = input("Inserisci il nome del concerto da cercare: ")
+            concerts = search_concerts_by_name(concert_name)
+            clear_screen()
+            display_concerts(concerts)
+            input("\nPremi INVIO per tornare al menu di ricerca...")
+
+        elif scelta == '3':
+            location_name = input("Inserisci la località del concerto da cercare: ")
+            concerts = search_concerts_by_location(location_name)
+            clear_screen()
+            display_concerts(concerts)
+            input("\nPremi INVIO per tornare al menu di ricerca...")
+
+        elif scelta == '4':
+            break
+
+        else:
+            print("\nOpzione non valida. Riprova.")
+            input("\nPremi INVIO per continuare...")
 
 # Funzione principale del menu
 def main_menu():
@@ -73,7 +119,7 @@ def main_menu():
         print("Benvenuto!")
         print("===========")
         print("Opzioni disponibili:")
-        print("1. Cerca concerti per artista")
+        print("1. Cerca concerti")
         print("2. Esci")
         
         scelta = input("\nInserisci il numero dell'opzione desiderata: ")
